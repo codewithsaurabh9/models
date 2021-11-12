835fe7b745b1a8c255516ce1e6935970f87ccf17
codewithsaurabh9 <mishrasaurabh0398@gmil.com>
2021-11-12 20:19:51 +0530

    host

--- models/settings.py
+++ models/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(4lzt_xq53zdwoaqo642&a5mk_b0x9&*7enw&%l$!(rzkyk4^i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
