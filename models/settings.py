6ad029428901d1efd859c090eeb1df5209e17e88
saurabh <88712236+codewithsaurabh9@users.noreply.github.com>
2021-11-12 20:54:11 +0530

    Update settings.py

--- models/settings.py
+++ models/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = 'staticfiles'
 # STATIC_URL = '/static/'
